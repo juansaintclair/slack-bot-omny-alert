069316e7b1fe30196e3f548ad2f6f9c8a4ab8217
Juan Saint Clair <juansaintclair@gmail.com>
2020-08-14 15:11:07 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const { App } = require('@slack/bolt');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+(async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
